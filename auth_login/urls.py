3560990877f5c3c27898d7a17e3f2f0489df1fcb
Bhushan Borse <borsebhushan216@gmail.com>
2023-04-18 12:03:26 +0530

    changes

--- auth_login/urls.py
+++ auth_login/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from auth_login import views
 
 urlpatterns = [
-    path('login/', views.login),
+    path('login/', views.login_user),
+    path('logout/', views.logout_user),
 ]
--- auth_login/views.py
+++ auth_login/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
-def login(request):
+def login_user(request):
     if request.method.lower() == "get":
+        if request.user.is_authenticated:
+            return redirect("/")
         return render(request, template_name="login.html")
+
+    user = authenticate(request, username=request.POST["username"], password=request.POST["password"])
+    if user is not None:
+        login(request, user)
+        return redirect("/")
+
+    return render(request, template_name="login.html", context={
+        "message": "Wrong Credentials!."
+    })
+
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return redirect("/login")
